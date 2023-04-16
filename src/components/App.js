51005f530867d035a9dacc41f9cdf6c73de0e953
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-04-17 01:01:19 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/App.css';
+import Header from './Header';
+const App = () => {
+
+  return (
+    <div id="main">
+    
+    </div>
+  )
+}
+
+
+export default App;
